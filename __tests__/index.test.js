a5e496deaf41355af356df2cb255eb395a6f8d40
Alina <linarogozhnikova30@gmail.com>
2024-10-02 03:58:20 +0500

    again dont understand what the mistake is

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -28,4 +28,10 @@ describe('test sylish', () => {
     expect(genDiff('file3.json', 'file4.json', 'json')).toBe(readFile('json.txt'));
     expect(genDiff('file3.yml', 'file4.yml', 'json')).toBe(readFile('json.txt'));
   });
+  test('non-existent files', () => {
+    expect(() => genDiff('nonexistent1.json', 'nonexistent2.json')).toThrow();
+  });
+  test('unsupported file format', () => {
+    expect(() => genDiff('file3.txt', 'file4.txt')).toThrow();
+  });
 });
