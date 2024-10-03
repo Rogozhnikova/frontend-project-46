2155b853bccb984c6664337f061c9277a6fad5fa
Alina <linarogozhnikova30@gmail.com>
2024-10-04 00:27:04 +0500

    fixed linter errors

--- src/findFile.js
+++ src/findFile.js
@@ -2,25 +2,25 @@ import fs from 'fs';
 import path from 'path';
 
 function findFile(searchPath, filename) {
-    const results = [];
-    function search(dir) {
-      const files = fs.readdirSync(dir);
-  
-      files.forEach((file) => {
-        const filePath = path.join(dir, file);
-        const stat = fs.statSync(filePath);
-  
-        if (stat && stat.isDirectory()) {
-          search(filePath);
-        } else if (file === filename) {
-          results.push(filePath);
-        }
-      });
-    }
-  
-    search(searchPath);
-  
-    return results[0];
+  const results = [];
+  function search(dir) {
+    const files = fs.readdirSync(dir);
+
+    files.forEach((file) => {
+      const filePath = path.join(dir, file);
+      const stat = fs.statSync(filePath);
+
+      if (stat && stat.isDirectory()) {
+        search(filePath);
+      } else if (file === filename) {
+        results.push(filePath);
+      }
+    });
   }
-  
-  export default findFile;
+
+  search(searchPath);
+
+  return results[0];
+}
+
+export default findFile;
