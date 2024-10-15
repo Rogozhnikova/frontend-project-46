44fb46c1bb2d3cf2bfafd153f89a87e90006ecf9
Alina <linarogozhnikova30@gmail.com>
2024-10-15 22:21:49 +0500

    fix

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ import process from 'process';
 import buildTree from './buildTree.js';
 import parse from './parsers.js';
 import formatter from './formaters/formatters.js';
-import findFile from './findFile.js';
 
 const genDiff = (fileName1, fileName2, format) => {
-  const pathToFile1 = findFile(process.cwd(), fileName1);
-  const pathToFile2 = findFile(process.cwd(), fileName2);
+  const pathToFile1 = path.resolve(process.cwd(), fileName1);
+  const pathToFile2 = path.resolve(process.cwd(), fileName2);
 
   const file1 = parse(readFileSync(pathToFile1), path.extname(fileName1));
   const file2 = parse(readFileSync(pathToFile2), path.extname(fileName2));
