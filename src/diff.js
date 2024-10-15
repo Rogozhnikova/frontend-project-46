0f6c41b23f559b93be87e8ae3fc75d0fcd03ecb2
Alina <linarogozhnikova30@gmail.com>
2024-10-15 22:32:30 +0500

    fix

--- src/diff.js
+++ src/diff.js
@@ -2,22 +2,29 @@ import _ from 'lodash';
 
 const compare = (file1, file2) => {
   const keys = [Object.keys(file1), Object.keys(file2)].flat();
-  const result = [];
 
-  keys.forEach((key) => {
+  const result = keys.reduce((acc, key) => {
     if (_.has(file1, key) && _.has(file2, key)) {
       if (file1[key] === file2[key]) {
-        result.push(`   ${key}:${file1[key]}`);
-      } else {
-        result.push(` - ${key}:${file1[key]}`);
-        result.push(` + ${key}:${file2[key]}`);
+        return [...acc, `   ${key}:${file1[key]}`];
       }
-    } else if (_.has(file1, key)) {
-      result.push(` - ${key}:${file1[key]}`);
-    } else if (_.has(file2, key)) {
-      result.push(` + ${key}:${file2[key]}`);
+      return [
+        ...acc,
+        ` - ${key}:${file1[key]}`,
+        ` + ${key}:${file2[key]}`,
+      ];
     }
-  });
+
+    if (_.has(file1, key)) {
+      return [...acc, ` - ${key}:${file1[key]}`];
+    }
+
+    if (_.has(file2, key)) {
+      return [...acc, ` + ${key}:${file2[key]}`];
+    }
+
+    return acc; // Возвращаем аккумулятор без изменений
+  }, []);
 
   return _.uniq(result).sort((a, b) => {
     if (a[3] < b[3]) return -1;
